e559b572b610e697038468c4fb27d879570390ed
eric <iieric@163.com>
2022-10-20 22:20:59 +0800

    习题1：围绕中心点旋转

--- opengl_demo/demo.cpp
+++ opengl_demo/demo.cpp
@@ -217,8 +217,9 @@ int main()
         
         // 使用glm库进行计算
         glm::mat4 transform = glm::mat4(1.0f);
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
 
         //unsigned int transformLoc = glGetUniformLocation(shaderProgram.getId(), "transform");
         //glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
